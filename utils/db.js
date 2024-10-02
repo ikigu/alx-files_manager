abc1b5ab150778a3fa711449c339ebbba168aeb4
George Ikigu <ikigu.geo@gmail.com>
2024-10-02 21:26:27 +0300

    feat: add mongodb client

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.DB_PORT || 27017;
+    const URL = `mongodb://${HOST}:${PORT}`;
+    this.client = new MongoClient(URL);
+
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.connected = false;
+
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
